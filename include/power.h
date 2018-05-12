5f301f6beb31b6d5b203ed3dee5a4b3b7262ab7e
Donnie Tolbert <donnie.tolbert@target.com>
2018-05-12 08:12:42 -0500

    split power

---
+++ include/power.h
@@ -0,0 +1,45 @@
+
+#include <string>
+#include <iostream>
+
+#include <libcec/cec.h>
+#include <libcec/cecloader.h>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+
+using namespace CEC;
+
+const char * DEFAULT_DEVICE_NAME = "Smart Mirror";
+
+class Power {
+private:
+  libcec_configuration g_config;
+  ICECAdapter *g_parser;
+  ICECCallbacks        g_callbacks;
+  std::string g_port;
+  string device_name;
+  bool verbose;
+  atomi_flag failed;
+
+
+private:
+  static void handleCecLogMessage(void * cbParam, const cec_log_message * message) {
+    reinterpret_cast<Power*>(cbParam)->CecLogMessage(cbParam, message);
+  }
+  void CecLogMessage(void *cbParam, const cec_log_message* message);
+
+  static void handleCecAlert(void * cbParam, const libcec_alert type, const libcec_parameter param) {
+    reinterpret_cast<Power*>(cbParam)->CecAlert(cbParam, type, param);
+  }
+  void CecAlert(void *cbParam, const libcec_alert type, const libcec_parameter param);
+
+  void init();
+public:
+  Power();
+  string get_port() const;
+  bool is_fail();
+  ~Power();
+};
--- src/power.cpp
+++ src/power.cpp
@@ -5,6 +5,8 @@
 #include <libcec/cec.h>
 #include <libcec/cecloader.h>
 
+#include "power.h"
+
 using std::cout;
 using std::cerr;
 using std::endl;
@@ -12,93 +14,73 @@ using std::string;
 
 using namespace CEC;
 
-const char * DEFAULT_DEVICE_NAME = "Smart Mirror";
 
-class Power {
-private:
-  libcec_configuration g_config;
-  ICECAdapter *g_parser;
-  ICECCallbacks        g_callbacks;
-  std::string g_port;
-  string device_name;
-  bool verbose;
-  atomi_flag failed;
+void Power::CecLogMessage(void *cbParam, const cec_log_message* message) {
+	cerr << message->message << endl;
+}
 
+void Power::CecAlert(void *cbParam, const libcec_alert type, const libcec_parameter param) {
+	switch (type) {
+	case CEC_ALERT_CONNECTION_LOST:
+		cerr << "connection lost, trying to reconnect\n" << endl;
+		if(g_parser) {
+			g_parser->Close();
+			if(!g_parser->Open(g_port.c_str())) {
+				cerr << "failed to reconnect.\n";
+        failed.test_and_set();
+			}
+		}
+	}
+}
 
-private:
-  static void handleCecLogMessage(void * cbParam, const cec_log_message * message) {
-    reinterpret_cast<Power*>(cbParam)->CecLogMessage(cbParam, message);
+void Power::init() {
+  g_config.Clear();
+  snprintf(g_config.strDeviceName, device_name.length(), device_name.c_str());
+  g_config.clientVersion      = LIBCEC_VERSION_CURRENT;
+  g_config.bActivateSource    = 1;
+  if (verbose) {
+    g_callbacks.logMessage      = &Power::handleCecLogMessage;
   }
-  void CecLogMessage(void *cbParam, const cec_log_message* message) {
-  	cerr << message->message << endl;
+  g_callbacks.alert           = &Power::handleCecAlert;
+  g_config.callbackParam = reinterpret_cast<void*>(this);
+  g_config.callbacks = &g_callbacks;
+  g_config.deviceTypes.Add(CEC_DEVICE_TYPE_PLAYBACK_DEVICE);
+
+  g_parser = LibCecInitialise(&g_config);
+
+  if (!g_parser) {
+    throw string("could not initialize libcec");
   }
 
-  static void handleCecAlert(void * cbParam, const libcec_alert type, const libcec_parameter param) {
-    reinterpret_cast<Power*>(cbParam)->CecAlert(cbParam, type, param);
+  g_parser->InitVideoStandalone();
+  cec_adapter_descriptor devices[10];
+  uint8_t iDevicesFound = g_parser->DetectAdapters(devices, 10, NULL, true);
+  if (iDevicesFound <= 0) {
+    throw string("no devices found");
   }
-  void CecAlert(void *cbParam, const libcec_alert type, const libcec_parameter param) {
-  	switch (type) {
-  	case CEC_ALERT_CONNECTION_LOST:
-  		cerr << "connection lost, trying to reconnect\n" << endl;
-  		if(g_parser) {
-  			g_parser->Close();
-  			if(!g_parser->Open(g_port.c_str())) {
-  				cerr << "failed to reconnect.\n";
-          failed.test_and_set();
-  			}
-  		}
-  	}
+  cout << "devices found:\n";
+  for(int i = 0; i < iDevicesFound; i++) {
+    cout << devices[i].strComName << endl;
   }
+  g_port = devices[0].strComName;
+  if (!g_parser->Open(g_port.c_str())) {
+    throw string("could not open device");
+  }
+}
 
-  void init() {
-    g_config.Clear();
-    snprintf(g_config.strDeviceName, device_name.length(), device_name.c_str());
-    g_config.clientVersion      = LIBCEC_VERSION_CURRENT;
-    g_config.bActivateSource    = 1;
-    if (verbose) {
-      g_callbacks.logMessage      = &Power::handleCecLogMessage;
-    }
-    g_callbacks.alert           = &Power::handleCecAlert;
-    g_config.callbackParam = reinterpret_cast<void*>(this);
-    g_config.callbacks = &g_callbacks;
-    g_config.deviceTypes.Add(CEC_DEVICE_TYPE_PLAYBACK_DEVICE);
-
-    g_parser = LibCecInitialise(&g_config);
-
-    if (!g_parser) {
-      throw string("could not initialize libcec");
-    }
-
-    g_parser->InitVideoStandalone();
-    cec_adapter_descriptor devices[10];
-    uint8_t iDevicesFound = g_parser->DetectAdapters(devices, 10, NULL, true);
-    if (iDevicesFound <= 0) {
-      throw string("no devices found");
-    }
-    cout << "devices found:\n";
-    for(int i = 0; i < iDevicesFound; i++) {
-      cout << devices[i].strComName << endl;
-    }
-    g_port = devices[0].strComName;
-    if (!g_parser->Open(g_port.c_str())) {
-      throw string("could not open device");
-    }
+Power::Power() : device_name(DEFAULT_DEVICE_NAME), verbose(false), failed(ATOMIC_FLAG_INIT) {
+  init();
+}
+string Power::get_port() const {
+  return g_port;
+}
+bool Power::is_fail() {
+  if (failed.test_and_set()) {
+    return true;
   }
-public:
-  Power() : device_name(DEFAULT_DEVICE_NAME), verbose(false), failed(ATOMIC_FLAG_INIT) {
-    init();
-  }
-  string get_port() const {
-    return g_port;
-  }
-  bool is_fail() {
-    if (failed.test_and_set()) {
-      return true;
-    }
-    failed.clear();
-    return false;
-  }
-  ~Power() {
-    UnloadLibCec(g_parser);
-  }
-};
+  failed.clear();
+  return false;
+}
+Power::~Power() {
+  UnloadLibCec(g_parser);
+}
